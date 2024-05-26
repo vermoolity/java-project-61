d7fc1165ed3e944c9454899f2e2dac5ece2e4947
Светлана Попова <pixeltiger@yandex.ru>
2024-05-27 03:02:07 +0500

    commit all

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    application
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
